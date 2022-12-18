f8bdd5f985cf4f2349814220c823dcb96678ea6f
Shylu <sshylesh34@gmail.com>
2022-12-18 11:39:10 +0530

    db-changes

--- server/database/db.js
+++ server/database/db.js
@@ -1,17 +1,19 @@
 import mongoose from "mongoose";
 
-const Connection = async (username, password) => {
-    const URL = `mongodb://${username}:${password}@blog-app-shard-00-00.t67bd.mongodb.net:27017,blog-app-shard-00-01.t67bd.mongodb.net:27017,blog-app-shard-00-02.t67bd.mongodb.net:27017/test?ssl=true&replicaSet=atlas-14nfap-shard-0&authSource=admin&retryWrites=true&w=majority`
+async function Connection() {
+    const URL = `mongodb://localhost:27017/blog-site`;
 
     try {
         await mongoose.connect(URL, {
             useNewUrlParser: true
-        })
+        });
         console.log("DB connected successfully");
     } catch (error) {
         console.log("error");
         console.log(error);
     }
 }
+
+
 
 export default Connection
