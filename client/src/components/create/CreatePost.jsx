b52373356efd49e02ad20304fdc98e5cab5ef074
Shylu <sshylesh34@gmail.com>
2022-12-18 21:27:45 +0530

    createPost-client

--- client/src/components/create/CreatePost.jsx
+++ client/src/components/create/CreatePost.jsx
@@ -1,18 +1,17 @@
 import { Box, FormControl, InputBase, styled, Button, TextareaAutosize } from "@mui/material"
 
+import { useState, useEffect, useContext } from "react"
+
 import { AddCircle } from "@mui/icons-material"
+import { API } from '../../service/api';
 
+import { DataContext } from '../../context/DataProvider';
+import { useNavigate, useLocation } from 'react-router-dom';
 
-const Container = styled(Box)`
+const Container = styled(Box)
+    `
     margin:50px 100px
 `
-
-const Image = styled("img")({
-    width: '100%',
-    height: "50vh",
-    objectFit: "cover"
-})
-
 
 const StyledFormControl = styled(FormControl)
     `
@@ -21,12 +20,14 @@ const StyledFormControl = styled(FormControl)
     flex-direction: row;
 `
 
-const InputTextField = styled(InputBase)`
+const InputTextField = styled(InputBase)
+    `
 flex:1;
 margin: 0 30px;
 font-size:20px;
 `
-const TextArea = styled(TextareaAutosize)`
+const TextArea = styled(TextareaAutosize)
+    `
 width:100%;
 margin-top:50px;
 font-size:18px;
@@ -37,27 +38,80 @@ border:none;
 
 `
 
+const Image = styled("img")({
+    width: '100%',
+    height: "50vh",
+    objectFit: "cover"
+})
+
+const initialPost = {
+    title: '',
+    description: '',
+    picture: '',
+    username: '',
+    categories: '',
+    createdDate: new Date()
+}
+
 
 const CreatePost = () => {
+    // const navigate = useNavigate();
+    const location = useLocation();
 
-    const URL = 'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'
+
+    const [post, setPost] = useState(initialPost);
+    const [file, setFile] = useState('');
+    const { account } = useContext(DataContext);
+
+    const URL = post.picture ? post.picture : 'https://images.unsplash.com/photo-1543128639-4cb7e6eeef1b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGFwdG9wJTIwc2V0dXB8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'
+
+
+    useEffect(() => {
+        const getImage = async () => {
+            if (file) {
+                const data = new FormData();
+                data.append("name", file.name);
+                data.append("file", file);
+
+                const response = await API.uploadFile(data);
+                post.picture = response.data;
+
+            }
+        }
+        getImage();
+        post.categories = location.search?.split('=')[1] || 'All';
+        post.username = account.username;
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [file])
+
+
+    const handleChange = (e) => {
+        console.log("Post started");
+        setPost({ ...post, [e.target.name]: e.target.value })
+    };
+
 
     return (
         <Container>
             <Image src={URL} alt="Post_images" />
 
             <StyledFormControl>
+
                 <label htmlFor="fileInput">
                     <AddCircle fontSize="large" color="action" />
                 </label>
+
                 <input
-                    type="file" name="" id="fileInput" style={{ display: "none" }} />
-                <InputTextField placeholder="Enter your Title" />
+                    type="file" name="" id="fileInput" style={{ display: "none" }} onChange={(e) => { setFile(e.target.files[0]) }} />
+
+                <InputTextField placeholder="Enter your Title" onChange={(e) => handleChange(e)} name="title" />
                 <Button variant="contained">Publish</Button>
             </StyledFormControl>
             <TextArea
                 minRows={5}
                 placeholder="...Enter Your Views..."
+                onChange={(e) => handleChange(e)} name="description"
             />
         </Container>
     )
